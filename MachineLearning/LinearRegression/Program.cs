a66ba2381cec6772fa35d73e23369a8698179eb4
SzilvasiPeter <peti.szilvasi95@gmail.com>
2021-10-02 12:08:24 +0200

    Refactoring to methods

--- MachineLearning/LinearRegression/Program.cs
+++ MachineLearning/LinearRegression/Program.cs
@@ -11,45 +11,53 @@ namespace LinearRegression
     {
         static void Main(string[] args)
         {
-            var frame = Frame.ReadCsv("ex1data1.txt");
-            var xSeries = frame.GetColumn<double>("X");
-            var ySeries = frame.GetColumn<double>("Y");
-            xSeries.Print();
-            ySeries.Print();
-            int numberOfExamples = ySeries.ValueCount;
-            Console.WriteLine(numberOfExamples);
-            //PlotSeries(xSeries, ySeries);
-
-            var intercept = Enumerable.Range(0, numberOfExamples).Select(i => KeyValue.Create(i, 1.0)).ToSeries();
-            List<Series<int, double>> xSeriesList = new List<Series<int, double>>()
-            {
-                xSeries,
-                intercept
-            };
-            var xFrame = Frame.CreateEmpty<int, string>();
-            xFrame.AddColumn("X", xSeries);
-            xFrame.AddColumn("Intercept", intercept);
-            xFrame.Print();
-            var thetaSeries = new Series<int, double>(new int[2] { 0, 1 }, new double[2]);
-            int iterations = 1500;
-            double learningRate = 0.01;
-
-            Vector<double> cost = CreateVector.Dense<double>(numberOfExamples, i => 0);
-            Vector<double> multiplier = CreateVector.Dense<double>(numberOfExamples, i => (1 / (2 * numberOfExamples)));
-
-            var X = Deedle.Math.Matrix.ofFrame(xFrame);
-            var y = Deedle.Math.Series.toVector(ySeries);
+            // Prepare data
+            var data = PrepareInputData("ex1data1.txt");
+            Matrix<double> X = data.X;
+            Vector<double> y = data.y;
             Console.WriteLine(X);
             Console.WriteLine(y);
 
-            // sum(((X*theta)-y).^2) * (1/(2*m));
-            var dotproduct = Deedle.Math.Matrix.dot(X, thetaSeries);
-            var dotproductMinusY = dotproduct - y;
-            var dotproductMinusYPower2 = dotproductMinusY.PointwisePower(2);
-            var dotproductMinusYPower2Sum = dotproductMinusYPower2.Sum();
-            var dotproductMinusYPower2SumDivided2M = dotproductMinusYPower2Sum * (1.0 / (2.0 * numberOfExamples));
-            var J = dotproductMinusYPower2SumDivided2M;
+            // ComputeCost
+            var thetaSeries = new Series<int, double>(new int[2] { 0, 1 }, new double[2]);
+            double J = ComputeCost(X, y, thetaSeries);
             Console.WriteLine(J);
+
+            // Gradient descent
+            int iterations = 1500;
+            double learningRate = 0.01;
+        }
+
+        public static (Matrix<double> X, Vector<double> y) PrepareInputData(string csvPath)
+        {
+            // Load from csv
+            var frame = Frame.ReadCsv(csvPath);
+
+            // Prepare X Matrix
+            Series<int, double> xSeries = frame.GetColumn<double>("X");
+            Series<int, double> intercept = Enumerable.Range(0, xSeries.ValueCount).Select(i => KeyValue.Create(i, 1.0)).ToSeries();
+            Frame<int, string> xFrame = Frame.CreateEmpty<int, string>();
+            xFrame.AddColumn("X", xSeries);
+            xFrame.AddColumn("Intercept", intercept);
+            Matrix<double> X = Deedle.Math.Matrix.ofFrame(xFrame);
+
+            // Preapare Y vector
+            Series<int, double> ySeries = frame.GetColumn<double>("Y");
+            Vector<double> y = Deedle.Math.Series.toVector(ySeries);
+
+            return (X, y);
+        }
+
+        public static double ComputeCost(Matrix<double> X, Vector<double> y, Series<int, double> theta)
+        {
+            // J = sum(((X*theta)-y).^2) * (1/(2*m))
+            var dotproduct = Deedle.Math.Matrix.dot(X, theta); // X*theta
+            var dotproductMinusY = dotproduct - y; // (X*theta) - y
+            var dotproductMinusYPower2 = dotproductMinusY.PointwisePower(2); // ((X*theta)-y).^2
+            var dotproductMinusYPower2Sum = dotproductMinusYPower2.Sum(); // sum(((X*theta)-y).^2)
+            var dotproductMinusYPower2SumDivided2M = dotproductMinusYPower2Sum * (1.0 / (2.0 * X.RowCount)); // sum(((X*theta)-y).^2)*(1/(2*m))
+
+            return dotproductMinusYPower2SumDivided2M;
         }
 
         public static void PlotSeries(Series<int, double> x, Series<int, double> y)
